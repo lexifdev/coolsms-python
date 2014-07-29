62f9aedb05fff13ab9a95f0fd9f41afb6982b29e
lexifdev <sl@lxf.kr>
2014-07-29 18:58:02 +0900

    init

---
+++ coolsms/sender.py
@@ -0,0 +1,44 @@
+import time
+import hashlib
+import hmac
+import uuid
+import requests
+
+
+class Sender(object):
+    _host = 'api.coolsms.co.kr'
+    _version = '1'
+
+    def __init__(self, key, secret):
+        self._key = key
+        self._secret = secret
+        self._base_url = 'https://{host}/{version}/'.format(
+            host=self._host,
+            version=self._version
+        )
+
+    def _auth_info(self):
+        timestamp = int(time.time())
+        salt = uuid.uuid4()
+        data = str(timestamp) + salt
+        hash = hmac.new(self._secret, data, hashlib.md5)
+
+        return timestamp, salt, hash.hexdigest()
+
+    def send(self, sender, receiver, text):
+        timestamp, salt, signature = self._auth_info()
+
+        params = {
+            'timestamp': timestamp,
+            'salt': salt,
+            'signature': signature,
+
+            'from': sender,
+            'to': receiver,
+            'text': text,
+        }
+        url = self._base_url + 'send'
+
+        response = requests.post(url, data=params)
+
+        return response.json()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name = 'coolsms',
+    version = '0.1',
+    packages = ['coolsms'],
+
+    install_requires = ['requests==2.3.0'],
+    author = 'lexifdev',
+    author_email = 'lexifdev@lxf.kr',
+    description = '''Simple python library for korean SMS service provider CoolSMS(coolsms.co.kr)'''
+)
